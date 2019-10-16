355615872dfb941cee84f4074ca5c21737a23c84
jihoon kim <jihoon0083@gmail.com>
2019-10-16 09:39:50 +0900

    안드로이드 지도관련

---
+++ app/src/main/java/com/kiki0083/kiki0083map/MapFragmentActivity.java
@@ -0,0 +1,125 @@
+package com.kiki0083.kiki0083map;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+
+public class MapFragmentActivity extends Activity implements OnMapReadyCallback {
+
+    private MapView mapview;
+    private LocationManager lm;
+
+    private   double x;
+    private   double y;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_naver);
+
+        mapview = findViewById(R.id.map_view);
+
+        //x = 37.588174;
+        //y = 127.22077;
+
+        // 권한 물어서 권한안되어있으면 권한 셋팅해주기
+        int permissionCheck1 = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        if(permissionCheck1 == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.INTERNET},1);
+
+        int permissionCheck2 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if(permissionCheck2 == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION},1);
+
+        int permissionCheck3 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if(permissionCheck3 == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+        // LocationManager 객체를 얻어온다
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록
+        try {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                    1, // 통지사이의 최소 변경거리 (m)
+                    mLocationListener);
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                    1, // 통지사이의 최소 변경거리 (m)
+                    mLocationListener);
+        } catch(SecurityException e){
+            e.printStackTrace();
+        }
+
+        mapview.getMapAsync(this);
+    }
+
+
+
+    @Override
+    public void onMapReady(@NonNull final NaverMap naverMap) {
+
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true);
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true);
+
+        System.out.println("x ----------->"+x);
+        System.out.println("y ----------->"+y);
+
+        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new LatLng(x, y));
+        naverMap.moveCamera(cameraUpdate);
+
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(x,y));
+        marker.setMap(naverMap);
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            //여기서 위치값이 갱신되면 이벤트가 발생한다.
+            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
+
+            Log.d("test", "onLocationChanged, location:" + location);
+            y = location.getLongitude(); //경도
+            x = location.getLatitude();   //위도
+        }
+
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+    };
+
+}
+
+
